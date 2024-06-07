189a090744ad08574ccf074ecc9965afa41d29be
AttilaGombosER <attila.gombos@effective-range.com>
2024-06-07 14:13:29 +0200

    initial code

---
+++ setup.py
@@ -0,0 +1,14 @@
+from setuptools import setup
+
+
+setup(
+    name='python-systemd-dbus',
+    version='1.3.2',
+    description='Python dbus interface library for systemd',
+    author='Ferenc Nandor Janky & Attila Gombos',
+    author_email='info@effective-range.com',
+    packages=['systemd_dbus'],
+    package_data={'systemd_dbus': ['py.typed']},
+    install_requires=['dbus-python',
+                      'python-context-logger@git+https://@github.com/EffectiveRange/python-context-logger.git@latest']
+)
