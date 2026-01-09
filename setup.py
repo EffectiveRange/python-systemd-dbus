0d66fa78cbc4cdbe9ceb4120161fcec22d92a991
Attila Gombos <attila.gombos@effective-range.com>
2026-01-09 17:28:58 +0100

    Update devcontainer, dependencies versioning

--- setup.py
+++ setup.py
@@ -3,12 +3,13 @@ from setuptools import setup
 
 setup(
     name='python-systemd-dbus',
-    version='1.5.1',
     description='Python dbus interface library for systemd',
     author='Ferenc Nandor Janky & Attila Gombos',
     author_email='info@effective-range.com',
     packages=['systemd_dbus'],
     package_data={'systemd_dbus': ['py.typed']},
+    use_scm_version=True,
+    setup_requires=["setuptools_scm"],
     install_requires=['dbus-python',
                       'python-context-logger@git+https://github.com/EffectiveRange/python-context-logger.git@latest']
 )
